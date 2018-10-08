40536a0490bb11a3dd4f4207e153ad82d4df2874
max ortman <mortman2299@jh-mba158-20.d214.org>
2018-10-08 14:31:12 -0500

    Initial Commit

---
+++ commision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commision
+//
+//  Created by max ortman on 10/8/18.
+//  Copyright © 2018 john hersey high school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
